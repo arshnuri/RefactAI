2a47419e74c33864592381f73f0d8e3434f0232e
mohammed arsh nuri <83339472+arshnuri@users.noreply.github.com>
2025-08-01 20:09:47 +0530

    Update t1.py

--- t1.py
+++ t1.py
@@ -1,19 +1,1 @@
-def calculate_positive_sum(numbers: list[int]) -> int:
-    """
-    Calculate the sum of positive numbers in a list.
 
-    Args:
-        numbers (list[int]): A list of integers.
-
-    Returns:
-        int: The sum of positive numbers in the list.
-    """
-    total = 0
-    for num in numbers:
-        if num > 0:
-            total += num
-    return total
-
-numbers = [1, -2, 3, 4, -1]
-result = calculate_positive_sum(numbers)
-print(result)
